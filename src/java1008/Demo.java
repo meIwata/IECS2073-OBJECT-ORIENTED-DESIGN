3b048b6466c48bb9a99762bd0d4406cec325d0ab
waleilulu <waleilulu@gmail.com>
2025-10-08 11:18:18 +0800

    新增 GoodBook 類別，實作 Publication 類別，並更新 Book 類別的 toString 方法

--- src/java1008/Demo.java
+++ src/java1008/Demo.java
@@ -2,6 +2,17 @@ package java1008;
 
 public class Demo {
     public static void main(String[] args) {
+        /*
+        GoodBook gb = (GoodBook) new Book(); // 強制轉型
+        System.out.println(gb.toString());
+         */
+
+        Book fb = new Book();
+//        System.out.println(((GoodBook)fb).shout());
+
+        System.out.println(fb instanceof Book);
+//        System.out.println(fb instanceof GoodBook);
+
     /*
 //    Publication pp = new Publication();
       Publication bb  = new Book();
@@ -14,17 +25,33 @@ public class Demo {
         int ii = (int) dd; // 強轉型成int
         System.out.println(d + " " + ii);
         */
+        /*
         Integer i = 5;
         System.out.println(System.identityHashCode(i)); // 印出物件的記憶體位址
         i = 7;
         System.out.println(System.identityHashCode(i));
+        */
+        /*
+        System.out.println(1+2);
+        System.out.println("1"+ " "+"2");
+         */
     }
 }
-abstract class Publication{
+
+abstract class Publication {
 
 }
-class Book extends Publication{
-    public String toString(){
+
+class Book extends Publication {
+    public String toString() {
+        return "This is a book";
+    }
+}
+class GoodBook extends Publication {
+    public String shout(){
+        return "I am good";
+    }
+    public String toString() {
         return "This is a book";
     }
 }
