df9c46554d1c163f1ebe6a70b700fbf4549435cf
waleilulu <waleilulu@gmail.com>
2025-10-13 15:59:53 +0800

    更新 App 類別，新增有參建構子以設定預設年份，並調整使用範例

--- src/java1013/App.java
+++ src/java1013/App.java
@@ -12,6 +12,13 @@ class Car {
     }
 
     // 有參建構子
+    Car(String make, String model) {
+        this.make = make;
+        this.model = model;
+        year = 2025; // 預設年份
+    }
+
+    // 有參建構子
     Car(String make, String model, int inputyear) { // 這裡的make, model, inputyear是區域變數
         this.make = make; // this.make指的是上面的 String make，區域變數assign給成員變數
         this.model = model;
@@ -26,11 +33,16 @@ class Car {
 
 public class App {
     public static void main(String[] args) {
-        Car myCar2 = new Car(); //  如果已經自己設定好有參建構子，Java就不會再幫你做空參數建構子
         Car myCar = new Car("null", "Civic", 2020); // 使用有參建構子來建立物件
-        System.out.println(myCar.make); // null
-        System.out.println(myCar.model); // null
-        System.out.println(myCar.year); // 0
+        Car myCar2 = new Car(); //  如果已經自己設定好有參建構子，Java就不會再幫你做空參數建構子
+        Car myCar3 = new Car("BMW", "M3"); // 使用有參建構子來建立物件
+//        System.out.println(myCar.make); // null
+//        System.out.println(myCar.model); // null
+//        System.out.println(myCar.year); // 0
+
+        System.out.println(myCar3.make);
+        System.out.println(myCar3.model);
+        System.out.println(myCar3.year);
 
 //        System.out.println("-------------------");
 //
