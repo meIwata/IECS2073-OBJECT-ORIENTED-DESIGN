af66097988aa3db3df321d4ce4a95b4c5ca69ee3
waleilulu <waleilulu@gmail.com>
2025-10-20 16:54:41 +0800

    新增 Car 類別的 startEngine 方法，並更新 main 方法以示範引擎啟動功能

--- src/java1020/Car.java
+++ src/java1020/Car.java
@@ -17,10 +17,21 @@ public class Car {
         this("BMW", "M3", 2025);
     }
 
+    void startEngine() {
+        System.out.println("The engine of " + year + " " + make + " " + model + " is starting.");
+    }
     public static void main(String[] args) {
-        Car myCar = new Car();
-        System.out.println("Make: " + myCar.make);
-        System.out.println("Model: " + myCar.model);
-        System.out.println("Year: " + myCar.year);
+//        Car myCar = new Car();
+//        System.out.println("Make: " + myCar.make);
+//        System.out.println("Model: " + myCar.model);
+//        System.out.println("Year: " + myCar.year);
+
+
+        Car myCar = new Car("Toyota", "Camry", 2020);
+        myCar.startEngine();
+
+        Car myCar2 = new Car("Toyota", "Camry", 2020);
+        myCar2.make = "Ford";
+        myCar.startEngine();
     }
 }
