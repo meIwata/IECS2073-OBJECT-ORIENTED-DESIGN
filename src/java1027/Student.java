251f083fed7d020238ca7f2cb366d5a88afb5026
waleilulu <waleilulu@gmail.com>
2025-10-27 15:54:18 +0800

    重構 Student 類別，將成員變數設為私有，並新增 getter 和 setter 方法

--- src/java1027/Student.java
+++ src/java1027/Student.java
@@ -1,17 +1,35 @@
 package java1027;
 
 public class Student {
-    String name;
-    int age;
+
+    // 私有的成員變數
+    private String name;
+    private int age;
 
     void joinClub() {
         System.out.println(name + "參加社團");
     }
 
+    // 公共的getter和setter方法
+    public String getName() {
+        return name;
+    }
 
-    public static void main(String[] args) {
-        Student tom = new Student();
-        tom.name = "Tom";
-        tom.age = 20;
+    public void setName(String name) {
+        if (name.length() < 10) {
+            this.name = name;
+        }
+
     }
 }
+
+class Main {
+    public static void main(String[] args) {
+        Student tom = new Student();
+//        tom.name = "Tom";
+        tom.setName("Tom");
+//        tom.age = 20;
+        System.out.println(tom.getName());
+    }
+}
+
