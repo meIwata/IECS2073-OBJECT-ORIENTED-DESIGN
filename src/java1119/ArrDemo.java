ba6a0f0e77522206be084c5dbf7ab2a452e3f4e9
waleilulu <waleilulu@gmail.com>
2025-11-19 13:20:23 +0800

    更新 ArrDemo 類別，調整示範程式碼並新增 String 比較功能

--- src/java1119/ArrDemo.java
+++ src/java1119/ArrDemo.java
@@ -1,56 +1,60 @@
 package java1119;
 
 public class ArrDemo {
-    public static void main(String[] args) {
-//        Integer[] arr = new Integer[10];
-//        System.out.println(arr[0] + ", " + arr[1]); // null, null
-//        System.out.println(arr[0].intValue() + ", " + arr[1]); // null, null
+    public static void main(String args[]) {
+        //Integer[] arr = new Integer[5]{1,2,3,4,5};
+        //int[] arr = new int[10];
+        //char[] arr = new char[10];
+        //System.out.printf(arr[0].intValue() + "," + arr[1]);
+        //System.out.printf(arr[0] + "," + arr[1]);
+		/*
+		Book[] arr = new Book[3];
+		arr[0] = new Book("A");
+		arr[1] = new Book("B");
+		arr[2] = new Book("C");
+		Book[] brr = arr;
+		System.out.println(arr + "," + brr);
+		System.out.println(brr[0] + "," + brr[1]);
+		brr[0].name = "foo";
+		System.out.println(arr[0] + "," + arr[1]);
+		System.out.println(brr[0] + "," + brr[1]);
+		brr[1].set(brr[0]);
+		System.out.println(brr[0] + "," + brr[1]);
+		*/
 
-//        int[] arr = new int[10];
-//        System.out.println(arr[0] + ", " + arr[1]); // 0, 0
+        String a = new String("foo");
+        String b = new String("foo");
 
-//        char[] arr = new char[10];
-//        System.out.println("[" + arr[0] + "], [" + arr[1] + "]"); // [ ], [ ]
-
-        Book[] arr = new Book[3];
-        arr[0] = new Book("A");
-        arr[1] = new GoodBook("B");
-        arr[2] = new Book("C");
-        Book[] brr = arr;
-        System.out.println(brr[0] + ", " + brr[1]);
-        brr[0].name = "foo";
-        System.out.println(arr[0] + ", " + arr[1]);
-        brr[0].set(brr[0]);
-        System.out.println(brr[0]+", "+brr[1]);
-    }
-}
-
-abstract class Publication {
-}
-
-class Book extends Publication {
-    public String name;
-
-    public Book(String n) {
-        name = n;
+        System.out.println((a == b) + "," + a.equals(b) + "," + (a.compareTo(b) == 0));
     }
 
-    public String toString() {
-        return name;
+    abstract class Publication {
     }
 
-    public void set(Book in) {
-        name = in.name;
-        in.name = in.name + "_used";
-    }
-}
+    class Book extends Publication {
+        public String name;
 
-class GoodBook extends Book {
-    public GoodBook(String n) {
-        super(n);
+        public Book() {
+            name = "book";
+        }
+
+        public Book(String n) {
+            name = n;
+        }
+
+        public String toString() {
+            return name;
+        }
+
+        public void set(Book in) {
+            name = in.name;
+            in.name = in.name + "_used";
+        }
     }
 
-    public String shout() {
-        return "Good Book: " + name;
+    class GoodBook extends Book {
+        public String shout() {
+            return "I am good";
+        }
     }
 }
