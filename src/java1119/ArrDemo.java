4f59d373b422b238b20d641c5efa0be288970a76
waleilulu <waleilulu@gmail.com>
2025-11-19 10:25:57 +0800

    新增 ArrDemo 類別，實現整數陣列的基本示範

---
+++ src/java1119/ArrDemo.java
@@ -0,0 +1,8 @@
+package java1119;
+
+public class ArrDemo {
+    public static void main(String[] args) {
+        Integer[] arr = new Integer[10];
+        System.out.println(arr[0] + ", " + arr[1]); // null, null
+    }
+}
