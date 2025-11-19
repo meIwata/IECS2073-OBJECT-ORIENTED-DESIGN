7f8d31071bf9d6286268b0ff44b71ea637b627a2
waleilulu <waleilulu@gmail.com>
2025-11-19 10:28:33 +0800

    更新 ArrDemo 類別，新增 char 陣列示範並註解掉舊有的 Integer 陣列示範

--- src/java1119/ArrDemo.java
+++ src/java1119/ArrDemo.java
@@ -2,7 +2,15 @@ package java1119;
 
 public class ArrDemo {
     public static void main(String[] args) {
-        Integer[] arr = new Integer[10];
-        System.out.println(arr[0] + ", " + arr[1]); // null, null
+//        Integer[] arr = new Integer[10];
+//        System.out.println(arr[0] + ", " + arr[1]); // null, null
+//        System.out.println(arr[0].intValue() + ", " + arr[1]); // null, null
+
+//        int[] arr = new int[10];
+//        System.out.println(arr[0] + ", " + arr[1]); // 0, 0
+
+        char[] arr = new char[10];
+        System.out.println("[" + arr[0] + "], [" + arr[1] + "]"); // [ ], [ ]
+
     }
 }
