6bfbfa72f3fb677b426e0b8d5879fda57e3a73e7
waleilulu <waleilulu@gmail.com>
2025-12-15 15:59:10 +0800

    更新 CollectionDemo 類別，示範使用 ArrayList 操作角色集合並顯示集合大小

--- src/java1215/CollectionDemo.java
+++ src/java1215/CollectionDemo.java
@@ -1,21 +1,23 @@
 package java1215;
 
-import java.util.HashSet;
-import java.util.LinkedHashSet;
-import java.util.Set;
-import java.util.TreeSet;
+import java.util.*;
 
 public class CollectionDemo {
     public static void main(String[] args) {
 //        Set<String> roles = new HashSet<>(); // 無順序性、查詢、加入、刪除的效率較高、允許放入一個 null 值
 //        Set<String> roles = new LinkedHashSet<String>(); // 有順序性(依照加入的先後順序)、查詢、加入、刪除的效率較高、允許放入一個 null 值
-        Set<String> roles = new TreeSet<String>(); // 有順序性(依照字母排序)、查詢、加入、刪除的效率較低、不允許放入 null 值
+//        Set<String> roles = new TreeSet<String>(); // 有順序性(依照字母排序)、查詢、加入、刪除的效率較低、不允許放入 null 值，如果加入一樣的元素，size 不會增加
+        List<String> roles = new ArrayList<>(); // 有順序性(依照加入的先後順序)、允許放入重複的元素、允許放入多個 null 值，size 會增加
+
 
         roles.add("Admin");
+        roles.add("User");
         roles.add("User");
         roles.add("Editor");
         roles.add("Student");
         roles.add("Teacher");
+
+        System.out.println(roles.size());
 
         for(String role : roles) {
             System.out.println(role);
