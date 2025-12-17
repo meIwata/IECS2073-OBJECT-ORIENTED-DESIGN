842309821baee179f89bd4cf41c60af03449adfa
waleilulu <waleilulu@gmail.com>
2025-12-17 11:54:33 +0800

    新增 TTTModel 類別，重構 Tic-Tac-Toe 遊戲邏輯並改善事件處理

--- src/java1217/TTTHandler.java
+++ src/java1217/TTTHandler.java
@@ -7,25 +7,35 @@ import java.awt.event.ActionListener;
 public class TTTHandler implements ActionListener {
     JButton[] cells;
     boolean turn;
+    TTTModel model;
 
     public TTTHandler(JButton[] _cells) {
         cells = _cells;
         turn = true;
+        model = new TTTModel(_cells);
     }
 
     public void actionPerformed(ActionEvent event) {
         JButton clickedCell = (JButton) event.getSource();
         for (int i = 0; i < TTTBoard.NUMBER_CELLS; i++) {
             if (cells[i] == clickedCell) {
-                if (turn) {
-                    cells[i].setLabel("O");
-                } else {
-                    cells[i].setLabel("X");
+                if (model.setCell(i, turn)) {
+                    turn = !turn;
                 }
-                cells[i].setEnabled(false);
-                turn = !turn;
                 break;
             }
+        }
+        int result = model.getGameCondition();
+        switch (result) {
+            case 0:
+                System.out.println("O wins!");
+                break;
+            case 1:
+                System.out.println("X wins!");
+                break;
+            case 3:
+                System.out.println("Game is Tied!");
+                break;
         }
     }
 }
--- src/java1217/TTTModel.java
+++ src/java1217/TTTModel.java
@@ -1,4 +1,47 @@
 package java1217;
 
+import javax.swing.*;
+
 public class TTTModel {
+    int[] cellStatus = {0, 0, 0, 0, 0, 0, 0, 0, 0};
+    JButton[] cells;
+
+    public TTTModel(JButton[] _cells) {
+        cells = _cells;
+    }
+
+    public boolean setCell(int index, boolean turn) {
+        boolean isSuccessSetting = false;
+        if (cellStatus[index] == 0) {
+            if (turn) {
+                cells[index].setLabel("O");
+                cellStatus[index] = 1;
+            } else {
+                cells[index].setLabel("X");
+                cellStatus[index] = 2;
+            }
+            isSuccessSetting = true;
+        }
+        return (isSuccessSetting);
+    }
+
+    public int getGameCondition() {
+        /*
+        0: O win
+        1: X win
+        2: Tie
+        3: Not yet finished
+        */
+        int result = 3;
+        if ((cellStatus[0] == cellStatus[1]) & (cellStatus[1] == cellStatus[2])) {
+            if (cellStatus[0] == 0) return 0;
+            else if (cellStatus[0] == 1) return 1;
+        }
+        for (int i = 1; i <= 9; i++) {
+            if (cellStatus[i] == 2) {
+                return 2;
+            }
+        }
+        return 3;
+    }
 }
