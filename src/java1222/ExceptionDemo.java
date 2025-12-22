ca24195ea2fdf4669c0e87819c6152007bac02c3
waleilulu <waleilulu@gmail.com>
2025-12-22 15:39:47 +0800

    更新 ExceptionDemo 類別，新增例外處理以捕獲 ArrayIndexOutOfBoundsException

--- src/java1222/ExceptionDemo.java
+++ src/java1222/ExceptionDemo.java
@@ -4,7 +4,16 @@ public class ExceptionDemo {
     public static void main(String[] args) {
         int scores[] = {85, 90, 78};
         System.out.println(scores.length);
-        scores[3]=100; // ArrayIndexOutOfBoundsException，runtime exception
-        System.out.println("end of program...");
+
+        try {
+            scores[3] = 100; // ArrayIndexOutOfBoundsException，runtime exception
+            System.out.println("end of try block");
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println(e.getMessage()); // 捕獲並處理例外，印出錯誤訊息
+            System.out.println("end of catch block");
+        } finally {
+            System.out.println("end of finally block");
+        }
+        System.out.println("end of program");
     }
 }
