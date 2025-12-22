94bdc8e9dae4ee78800cc45161cdf3384842acc2
waleilulu <waleilulu@gmail.com>
2025-12-22 15:45:16 +0800

    更新 ExceptionDemo 類別，新增除以零的例外處理並修正陣列索引以避免 ArrayIndexOutOfBoundsException

--- src/java1222/ExceptionDemo.java
+++ src/java1222/ExceptionDemo.java
@@ -2,11 +2,25 @@ package java1222;
 
 public class ExceptionDemo {
     public static void main(String[] args) {
+
+        try {
+            int result = 3 / 0;
+            System.out.println("end of try block");
+        } catch (ArithmeticException e) {
+            System.out.println(e.getMessage());
+            System.out.println("end of catch block");
+        } finally {
+            System.out.println("end of finally block");
+        }
+
+        System.out.println("end of program...");
+
+        /*
         int scores[] = {85, 90, 78};
         System.out.println(scores.length);
 
         try {
-            scores[2] = 100; // ArrayIndexOutOfBoundsException，runtime exception
+            scores[3] = 100; // ArrayIndexOutOfBoundsException，runtime exception
             System.out.println("end of try block");
         } catch (ArrayIndexOutOfBoundsException e) {
             System.out.println(e.getMessage()); // 捕獲並處理例外，印出錯誤訊息
@@ -14,6 +28,8 @@ public class ExceptionDemo {
         } finally {
             System.out.println("end of finally block");
         }
-        System.out.println("end of program");
+        System.out.println("end of program...");
+        */
+
     }
 }
