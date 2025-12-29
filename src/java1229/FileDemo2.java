b32d58c3cc0ec309b45601f71ddb630eecd62832
waleilulu <waleilulu@gmail.com>
2025-12-29 16:26:32 +0800

    新增 data.csv 檔案並更新 FileDemo2 類別以讀取並顯示檔案內容

--- src/java1229/FileDemo2.java
+++ src/java1229/FileDemo2.java
@@ -1,10 +1,24 @@
 package java1229;
 
-import java.io.File;
+import java.io.*;
 
 public class FileDemo2 {
     public static void main(String[] args) {
-        File folder = new File("testDir");
-        folder.mkdir(); // 建立一個目錄
+//        File folder = new File("testDir");
+//        folder.mkdir(); // 建立一個目錄
+        try {
+            FileReader f = new FileReader("./testDir/data.csv");
+            BufferedReader br = new BufferedReader(f);
+            String line = "";
+            while ((line = br.readLine()) != null) {
+                System.out.println(line);
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println(e.getMessage());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+
     }
 }
